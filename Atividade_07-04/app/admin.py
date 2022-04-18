c58ee0044c126ea6cd74a3087aa13091f70a4fde
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-04-18 17:52:29 -0300

    Finalizando atividade

--- Atividade_07-04/app/admin.py
+++ Atividade_07-04/app/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import Pessoa, Evento, EventoCientifico
-
-@admin.register(Pessoa)
-class PessoaAdmin(admin.ModelAdmin):
-    pass
+from .models import Evento, EventoCientifico, Autor, PessoaJuridica, PessoaFisica, ArtigoCientifico
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
@@ -12,3 +8,20 @@ class EventoAdmin(admin.ModelAdmin):
 @admin.register(EventoCientifico)
 class EventoCientificoAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(ArtigoCientifico)
+class ArtigoCientificoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PessoaJuridica)
+class PessoaJuridicaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PessoaFisica)
+class PessoaFisicaAdmin(admin.ModelAdmin):
+    pass
+
--- Atividade_07-04/app/models.py
+++ Atividade_07-04/app/models.py
@@ -9,6 +9,18 @@ class Pessoa(models.Model):
     def __str__(self):
         return self.nome
 
+class Autor(Pessoa):
+    curriculo = models.CharField('Currículo', max_length=128, null=True)
+
+class PessoaJuridica(Pessoa):
+    cnpj = models.CharField('CNPJ', max_length=20)
+    razaoSocial = models.CharField('Razão Social', max_length=150)
+    def __str__(self):
+        return self.razaoSocial
+
+class PessoaFisica(Pessoa):
+    cpf = models.CharField('CPF', max_length=20)
+
 class Evento(models.Model):
     nome = models.CharField('Nome', max_length=50, null=True)
     eventoPrincipal = models.TextField('Evento Principal', null=True)
@@ -30,31 +42,11 @@ class EventoCientifico(Evento):
 
     def __str__(self):
         return self.nome
-     
-'''class PessoaJuridica(Pessoa):
-    cnpj = ""
-    razaoSocial = ""
 
-class PessoaFisica(Pessoa):
-    cpf = ""
+class ArtigoCientifico(models.Model):
+    titulo = models.CharField('Título', max_length=128, null=True)
+    autores = models.ManyToManyField(Autor)
+    evento = models.ForeignKey(EventoCientifico, null=True, on_delete=models.CASCADE)
 
-class Autor(Pessoa):
-    curriculo = ""
-    artigos = []
-
-class ArtigoCientifico:
-    titulo = ""
-    autores = []
-    evento = EventoCientifico()
-'''
-
-
-'''class Pessoa(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
-    nome = models.CharField('Nome', max_length=128)
-    data_de_nascimento = models.DateField('Data de nascimento', blank=True, null=True)
-    telefone_celular = models.CharField('Telefone celular', max_length=15, help_text='Número do telefone celular no formato (99) 99999-9999', null=True, blank=True)
-    telefone_fixo = models.CharField('Telefone fixo', max_length=14, help_text='Número do telefone fixo no formato (99) 9999-9999', null=True, blank=True)
-    email = models.EmailField('E-mail', null=True, blank=True)
     def __str__(self):
-        return self.nome'''
+        return self.titulo
