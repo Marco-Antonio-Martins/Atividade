f1f493c67973bea1e52458452a2edae7b3faa0f9
Marco Antonio Martins Porto Netto <marcomartins06@rede.ulbra.br>
2022-04-28 06:08:54 -0300

    Padronizando para a Web

--- Atividade_07-04/app/models.py
+++ Atividade_07-04/app/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField('Nome', max_length=128)
-    email = models.EmailField('E-mail', null=True, blank=True)
+    email = models.EmailField('E-mail')
     def __str__(self):
         return self.nome
 
 class Autor(Pessoa):
-    curriculo = models.CharField('Currículo', max_length=128, null=True)
+    curriculo = models.TextField('Currículo')
 
 class PessoaJuridica(Pessoa):
     cnpj = models.CharField('CNPJ', max_length=20)
@@ -22,17 +22,17 @@ class PessoaFisica(Pessoa):
     cpf = models.CharField('CPF', max_length=20)
 
 class Evento(models.Model):
-    nome = models.CharField('Nome', max_length=50, null=True)
-    eventoPrincipal = models.TextField('Evento Principal', null=True)
-    sigla = models.CharField('Sigla', max_length=10, null=True)
-    dataEHoraDeInicio = models.DateTimeField('Data e hora de início', default=None, null=True)
-    palavrasChave = models.CharField('Palavras chave', max_length=150, blank=True, null=True)
-    logotipo = models.CharField('Logotipo', max_length=50, blank=True, null=True)
+    nome = models.CharField('Nome', max_length=150)
+    eventoPrincipal = models.TextField('Evento Principal', max_length=150)
+    sigla = models.CharField('Sigla', max_length=50)
+    dataEHoraDeInicio = models.DateTimeField('Data e hora de início')
+    palavrasChave = models.CharField('Palavras chave', max_length=150)
+    logotipo = models.CharField(max_length=300)
     realizador = models.ForeignKey(Pessoa, on_delete=models.CASCADE, null=True)
-    cidade = models.CharField('Cidade', max_length=50, blank=True, null=True)
-    uf = models.CharField('UF', max_length=20, blank=True, null=True)
-    endereco = models.CharField('Endereço', max_length=50, null=True)
-    cep = models.CharField('CEP', max_length=15, blank=True, null=True)
+    cidade = models.CharField('Cidade', max_length=100)
+    uf = models.CharField('UF', max_length=2)
+    endereco = models.CharField('Endereço', max_length=200)
+    cep = models.CharField('CEP', max_length=15)
 
     def __str__(self):
         return self.nome
@@ -44,9 +44,9 @@ class EventoCientifico(Evento):
         return self.nome
 
 class ArtigoCientifico(models.Model):
-    titulo = models.CharField('Título', max_length=128, null=True)
+    titulo = models.CharField('Título', max_length=128)
     autores = models.ManyToManyField(Autor)
-    evento = models.ForeignKey(EventoCientifico, null=True, on_delete=models.CASCADE)
+    evento = models.ForeignKey(EventoCientifico, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
